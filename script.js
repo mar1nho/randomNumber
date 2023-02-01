a0a123380fed03ef356df4415de9e6649009970e
mar1nho <gustavomarinhosom@gmail.com>
2023-01-31 22:42:59 -0300

    last

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 let randomNum = 0;
 
 function numRand(){
-    randomNum = Math.floor(Math.random() * 10) + 1
+    randomNum = Math.floor(Math.random() * 100) + 1
     document.getElementById('numGer').innerHTML = `Number generated, now guess!`;
+    document.getElementById('userNum').readOnly = false;   
+    document.getElementById('res').innerHTML = 'Guess the Number.'
         console.log(randomNum)
-        document.getElementById('res').innerHTML = 'Guess the Number.'
 }
 function guessNum(){
     let num = document.getElementById('userNum').value;
@@ -16,7 +17,7 @@ function guessNum(){
             randomNum = 0
         } else {
             let tryAgain = ['Try again, buddy!', 'Still not the correct number', 'Next one, maybe...', 'Too close, but too far...', 'I believe in you!', 'Keep trying','Almost there brother...'];
-            document.getElementById('userNum').value = ''
+                document.getElementById('userNum').value = ''
             const randomQuo = tryAgain[Math.floor(Math.random() * tryAgain.length)]
                 document.getElementById('res').innerHTML = `${randomQuo}`
         }
